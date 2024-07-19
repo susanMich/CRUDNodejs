8025bef61d1aff39a2ff7be2e618d91010a36a6e
susanMich <michiales99@gmail.com>
2024-07-18 22:31:43 -0500

    nuevo

--- BD/estudiantes.sql
+++ BD/estudiantes.sql
@@ -11,5 +11,5 @@ CREATE TABLE `estudiantes` (
 ) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-INSERT INTO `estudiantes` VALUES (35,'Hola','53453go@gmail.com','PHP','2023-12-07'),(36,'Carlos x','667567@gmail.com','Python','2023-12-07'),(37,'nuevo','n@gmail.com','Python','2023-12-07'),(39,'nuevooo','n@gmail.com','PHP','2023-12-07'),(40,'nuevodsadasd','1eqwe12311@gmail.com','NodeJS','2023-12-07');
+INSERT INTO `estudiantes` VALUES (35,'Andrea Aviles','andreaaviles@gmail.com','PHP','2023-12-07'),(36,'Ericka Bravo','erickabravo@gmail.com','Python','2023-12-07'),(37,'Yucsan Aviles','yucsannnav@gmail.com','Python','2023-12-07'),(39,'Michell Aviles','n@gmail.com','PHP','2023-12-07'),(40,'Antonia Litardo','1eqwe12311@gmail.com','NodeJS','2023-12-07');
 
